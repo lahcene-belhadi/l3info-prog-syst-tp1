97a20febca8a74d47aa96f661d3ee718995a677d
lahcene-belhadi <lahcene.belhadi@alumni.univ-avignon.fr>
2021-10-27 16:38:55 +0200

    Add readme

---
+++ README.md
@@ -0,0 +1,26 @@
+# TP1
+## Compile tp1
+
+`$ make alldb`
+
+## Execute tp1
+
+`$ bin/tp1db`
+
+# Minibash
+## Compile Minibash
+
+`$ make minibash`
+
+## Execute Minibash
+
+`$ bin/minibash`
+
+# ExecFile
+## Compile ExecFile
+
+`$ make execfile`
+
+## Execute ExecFile
+
+`$ bin/execfile`
--- execfile.c
+++ execfile.c
@@ -1,21 +1,18 @@
 
-#include "header/tp1.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+
+#include "header/tp1.h"
 
 #define MAX_LEN 100
 
 
 int main(int argc, char* argv[]) {
 
-    char* filename = (char*) malloc(MAX_LEN * sizeof(char));
-
-    if (argc > 1) {
-
-        filename = argv[1];
-        exec_file_batch(filename);
+    if (argc > 1) 
+        exec_file_batch(argv[1]);
     
-    } else 
+    else 
         printf("Error: No filename provided.\n");
     
     return 0;
